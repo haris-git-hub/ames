3a36e49f80639a32afb4baf458f57d1f16954f51
haris <haris@gmail.com>
2019-04-19 10:52:01 +1200

    generate war

--- src/main/java/com/ames/AmesApplication.java
+++ src/main/java/com/ames/AmesApplication.java
@@ -3,11 +3,43 @@ package com.ames;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//for war
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+// for jar
+//@SpringBootApplication
+//public class AmesApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(AmesApplication.class, args);
+//	}
+//
+//}
+
+
+//for war
 @SpringBootApplication
-public class AmesApplication {
+public class AmesApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AmesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AmesApplication.class, args);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(AmesApplication.class);
+    }
 }
+
+
+
+
+
+
+
+
+
+
